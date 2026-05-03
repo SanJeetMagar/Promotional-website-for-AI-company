0a2025247b3802423163a8bdd906e77edfb227e5
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-03 13:17:22 +0545

    Added admin.py on logo

--- src/apps/logo/admin.py
+++ src/apps/logo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Logo
+@admin.register(Logo)
+class LogoAdmin(admin.ModelAdmin):
+    list_display = ('image', 'created_at')
+    search_fields = ('image',)
+    list_filter = ('created_at',)
