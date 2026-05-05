21e4b4e82498862826559e436bb9d2c961816e0e
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-05 22:00:32 +0545

    Added comment api

--- src/apps/blog/urls.py
+++ src/apps/blog/urls.py
@@ -1,6 +1,7 @@
-from .views import BlogPostListView
+from .views import BlogPostListView, CommentCreateView
 from django.urls import path, include
 
 urlpatterns = [
     path('posts/', BlogPostListView.as_view(), name='blogpost-list'),
+    path('comments/', CommentCreateView.as_view(), name='comment-create'),
 ]
--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -1,5 +1,5 @@
-from .models import BlogPost
-from .serializers import BlogPostSerializer
+from .models import BlogPost, Comment
+from .serializers import BlogPostSerializer, CommentSerializer
 from rest_framework import generics
 from drf_spectacular.utils import extend_schema 
 
@@ -8,3 +8,7 @@ class BlogPostListView(generics.ListAPIView):
     queryset = BlogPost.objects.filter(is_active=True)
     serializer_class = BlogPostSerializer   
 
+@extend_schema(description="Post a new comment", tags=['Comment'],summary="Create a comment")
+class CommentCreateView(generics.CreateAPIView):
+    serializer_class = CommentSerializer    
+    
