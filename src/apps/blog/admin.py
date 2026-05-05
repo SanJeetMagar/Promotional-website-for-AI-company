dc1ea71e88abc63fa0fd499fdaf85d565e455c10
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-05 21:56:07 +0545

    Added admin.py for blog

--- src/apps/blog/admin.py
+++ src/apps/blog/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
 from .models import BlogPost, Comment
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'is_active')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('order',)   
+
+
+    
