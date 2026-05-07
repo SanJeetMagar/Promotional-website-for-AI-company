c4b02c5828870745409aba5aa0e07ba36a8b6bbf
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-07 14:07:57 +0545

    changes on admin.py in team

--- src/apps/team/admin.py
+++ src/apps/team/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import TeamMember
+from .models import TeamMember, CEOMessage
 
 @admin.register(TeamMember)
 class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'position')
     search_fields = ('name', 'position')
+
+@admin.register(CEOMessage)
+class CEOMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'is_active')
+    search_fields = ('message',)
