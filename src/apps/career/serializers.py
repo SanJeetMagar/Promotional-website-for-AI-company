ba0e588a86cef33735c94b7c9bba14169831226e
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-07 17:58:24 +0545

    Serializer updated on career

--- src/apps/career/serializers.py
+++ src/apps/career/serializers.py
@@ -1,7 +1,18 @@
-from .models import Career
+from .models import CV, JobPosting
 from rest_framework import serializers
 
-class CareerSerializer(serializers.ModelSerializer):
+
+class CVSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Career
-        fields = '__all__'
+        model = CV
+        fields = ['id', 'full_name', 'email', 'phone_number', 'years_of_experience', 'linkedin_profile', 'github_profile', 'portfolio_link', 'resume_file', 'short_note',]
+
+class JobPostingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobPosting
+        fields = ['id', 'title', 'slug', 'location', 'tags', 'employment_type', 'work_arrangement', 'status']
+class JobPostingDetailSerializer(serializers.ModelSerializer):
+    tag = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = JobPosting
+        fields = ['id', 'title', 'slug', 'location', 'tags', 'description', 'deadline', 'seat_openings', 'required_skills', 'benefits', 'how_to_apply', 'employment_type', 'work_arrangement', 'status']
