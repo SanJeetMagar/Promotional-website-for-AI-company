12724bc9685d289b02dbbd33c01a027b9840f4e3
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-07 13:57:10 +0545

    Changes on views.py of team list to retrieve for ceomessageview

--- src/apps/team/views.py
+++ src/apps/team/views.py
@@ -2,16 +2,16 @@ from .models import TeamMember, CEOMessage
 from .serializers import TeamMemberSerializer, CEOMessageSerializer
 from rest_framework import generics     
 from drf_spectacular.utils import extend_schema
-
+from django.shortcuts import get_object_or_404
 @extend_schema(summary="Display team member",description="Get list of team members", tags=['Team Members'])
 class TeamMemberListView(generics.ListAPIView):
     serializer_class = TeamMemberSerializer 
     def get_queryset(self):
         return TeamMember.objects.filter(is_active=True)
 @extend_schema(summary="Active CEO Messages", description="Get list of active CEO messages", tags=['CEO Messages'])
-class CEOMessageListView(generics.ListAPIView):
-    def get_queryset(self):
-        return CEOMessage.objects.filter(is_active=True)
-    serializer_class = CEOMessageSerializer     
+class CEOMessageView(generics.RetrieveAPIView):
+    serializer_class = CEOMessageSerializer
 
+    def get_object(self):
+        return get_object_or_404(CEOMessage, is_active=True)
 
