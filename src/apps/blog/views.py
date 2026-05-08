4ffa88954f766aa98e756c00e97464f437c67c12
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-08 15:32:51 +0545

    Added APIview for BlogPostListView

--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -3,16 +3,22 @@ from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
 from .models import BlogPost, Comment, BlogStatus
 from .serializers import BlogPostListSerializer, BlogPostDetailSerializer, CommentSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 @extend_schema(tags=['Blog'], summary="List published blog posts")
-class BlogPostListView(generics.ListAPIView):
-    serializer_class = BlogPostListSerializer
-    def get_queryset(self):
-        return BlogPost.objects.filter(
-            status=BlogStatus.PUBLISHED
-        ).prefetch_related('comments')
-
+class BlogPostListView(APIView):
+    def get(self, request):
+        posts = BlogPost.objects.filter(status=BlogStatus.PUBLISHED).prefetch_related('tags')
+        featured_posts = posts.filter(is_featured=True)[:5]
+        latest_posts = posts.order_by('-published_date')[:10]      
+        return Response(
+        {
+            'all_posts': BlogPostListSerializer(posts, many=True).data,
+            'featured': BlogPostListSerializer(featured_posts, many=True).data,
+            'latest': BlogPostListSerializer(latest_posts, many=True).data
+        })
 
 @extend_schema(tags=['Blog'], summary="Get blog post details")
 class BlogPostDetailView(generics.RetrieveAPIView):
