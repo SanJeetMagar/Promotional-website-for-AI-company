02dbb406c5704155621a8505b381e61b9044a14d
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-11 22:21:19 +0545

    added about section

--- src/apps/about/models.py
+++ src/apps/about/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from src.apps.common.models import BaseModel  
+
+class About(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about_images/')
+
+    def __str__(self):
+        return self.title
+    
+
+class VisionMission(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
