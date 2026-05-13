8db865f95e4d7bdd0d8e157d6ad6572c73319e44
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-13 22:33:21 +0545

    Commit out all serializers.py

--- src/apps/about/serializers.py
+++ src/apps/about/serializers.py
@@ -1,12 +1,12 @@
-from .models import About, VisionMission
-from rest_framework import serializers  
+# from .models import About, VisionMission
+# from rest_framework import serializers  
 
-class AboutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = About
-        fields = ['id', 'title', 'description', 'image']    
+# class AboutSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = About
+#         fields = ['id', 'title', 'description', 'image']    
 
-class VisionMissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VisionMission
-        fields = ['id', 'title', 'description'] 
+# class VisionMissionSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = VisionMission
+#         fields = ['id', 'title', 'description'] 
