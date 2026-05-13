7dd63046090c74d775d69e033af18ab95b6f4881
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-05-13 22:33:46 +0545

    comment out all models.py

--- src/apps/about/models.py
+++ src/apps/about/models.py
@@ -1,18 +1,18 @@
-from django.db import models
-from src.apps.common.models import BaseModel  
+# from django.db import models
+# from src.apps.common.models import BaseModel  
 
-class About(BaseModel):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.ImageField(upload_to='about_images/')
+# class About(BaseModel):
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     image = models.ImageField(upload_to='about_images/')
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
     
 
-class VisionMission(BaseModel):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+# class VisionMission(BaseModel):
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
