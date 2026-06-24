38dea1e9563c721603932c4771528af157c4d795
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-06-24 20:50:29 +0545

    fixed all the endpoint

--- src/apps/blog/urls.py
+++ src/apps/blog/urls.py
@@ -1,26 +1,10 @@
 from django.urls import path
 from .views import (
-    # Blog Post Views
-    BlogPostListView,
-    BlogPostDetailView,
-    BlogPostCreateView,
-    BlogPostUpdateView,
-    BlogPostDeleteView,
-    BlogPostPublishView,
-    BlogPostArchiveView,
-    # Comment Views
-    CommentListView,
-    CommentCreateView,
-    CommentUpdateView,
-    CommentDeleteView,
-    # Tag Views
-    TagListView,
-    TagCreateView,
-    TagUpdateView,
-    TagDeleteView,
-    # Image Views
-    BlogImageCreateView,
-    BlogImageDeleteView,
+    BlogPostListView, BlogPostDetailView, BlogPostCreateView,
+    BlogPostUpdateView, BlogPostDeleteView, BlogPostPublishView,
+    BlogPostArchiveView, CommentListView, CommentCreateView,
+    CommentUpdateView, CommentDeleteView, TagListView, TagCreateView,
+    TagUpdateView, TagDeleteView, BlogImageCreateView, BlogImageDeleteView,
 )
 
 app_name = 'blog'
@@ -30,24 +14,24 @@ urlpatterns = [
     path('posts/', BlogPostListView.as_view(), name='post-list'),
     path('posts/create/', BlogPostCreateView.as_view(), name='post-create'),
     path('posts/<slug:slug>/', BlogPostDetailView.as_view(), name='post-detail'),
-    path('posts/update/', BlogPostUpdateView.as_view(), name='post-update'),
-    path('posts/delete/', BlogPostDeleteView.as_view(), name='post-delete'),
-    path('posts/publish/', BlogPostPublishView.as_view(), name='post-publish'),
-    path('posts/archive/', BlogPostArchiveView.as_view(), name='post-archive'),
-    
+    path('posts/<slug:slug>/update/', BlogPostUpdateView.as_view(), name='post-update'),
+    path('posts/<slug:slug>/delete/', BlogPostDeleteView.as_view(), name='post-delete'),
+    path('posts/<slug:slug>/publish/', BlogPostPublishView.as_view(), name='post-publish'),
+    path('posts/<slug:slug>/archive/', BlogPostArchiveView.as_view(), name='post-archive'),
+
     # Comment URLs
-    path('posts/comments/', CommentListView.as_view(), name='comment-list'),
-    path('posts/comments/create/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-    
+    path('posts/<slug:slug>/comments/', CommentListView.as_view(), name='comment-list'),
+    path('posts/<slug:slug>/comments/create/', CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+
     # Tag URLs
     path('tags/', TagListView.as_view(), name='tag-list'),
     path('tags/create/', TagCreateView.as_view(), name='tag-create'),
-    path('tags/update/', TagUpdateView.as_view(), name='tag-update'),
-    path('tags/delete/', TagDeleteView.as_view(), name='tag-delete'),
-    
+    path('tags/<slug:slug>/update/', TagUpdateView.as_view(), name='tag-update'),
+    path('tags/<slug:slug>/delete/', TagDeleteView.as_view(), name='tag-delete'),
+
     # Image URLs
-    path('posts/images/upload/', BlogImageCreateView.as_view(), name='image-upload'),
-    path('images/delete/', BlogImageDeleteView.as_view(), name='image-delete'),
+    path('posts/<slug:slug>/images/upload/', BlogImageCreateView.as_view(), name='image-upload'),
+    path('images/<int:pk>/delete/', BlogImageDeleteView.as_view(), name='image-delete'),
 ]
--- src/apps/events/urls.py
+++ src/apps/events/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from .views import (
-    # Event Views
     EventListView, EventDetailView, EventCreateView,
     EventUpdateView, EventDeleteView,
-    # Speaker Views
     SpeakerListView, SpeakerDetailView, SpeakerCreateView,
     SpeakerUpdateView, SpeakerDeleteView,
-    # Tag Views
     EventTagListView, EventTagCreateView, EventTagUpdateView,
     EventTagDeleteView,
-    # Registration Views
     EventRegistrationCreateView, EventRegistrationListView,
     EventRegistrationDetailView, EventRegistrationCancelView,
     EventRegistrationConfirmView,
-    # Gallery Views
     EventGalleryCreateView, EventGalleryDeleteView,
 )
 
@@ -24,30 +19,30 @@ urlpatterns = [
     path('', EventListView.as_view(), name='event-list'),
     path('create/', EventCreateView.as_view(), name='event-create'),
     path('<slug:slug>/', EventDetailView.as_view(), name='event-detail'),
-    path('update/', EventUpdateView.as_view(), name='event-update'),
-    path('delete/', EventDeleteView.as_view(), name='event-delete'),
-    
+    path('<slug:slug>/update/', EventUpdateView.as_view(), name='event-update'),       # Bug 1
+    path('<slug:slug>/delete/', EventDeleteView.as_view(), name='event-delete'),       # Bug 1
+
     # Speaker URLs
     path('speakers/', SpeakerListView.as_view(), name='speaker-list'),
     path('speakers/create/', SpeakerCreateView.as_view(), name='speaker-create'),
-    path('speakers/<int:pk>/', SpeakerDetailView.as_view(), name='speaker-detail'),
-    path('speakers/<int:pk>/update/', SpeakerUpdateView.as_view(), name='speaker-update'),
-    path('speakers/<int:pk>/delete/', SpeakerDeleteView.as_view(), name='speaker-delete'),
-    
+    path('speakers/<slug:slug>/', SpeakerDetailView.as_view(), name='speaker-detail'),         # Bug 2
+    path('speakers/<slug:slug>/update/', SpeakerUpdateView.as_view(), name='speaker-update'),  # Bug 2
+    path('speakers/<slug:slug>/delete/', SpeakerDeleteView.as_view(), name='speaker-delete'),  # Bug 2
+
     # Tag URLs
     path('tags/', EventTagListView.as_view(), name='tag-list'),
     path('tags/create/', EventTagCreateView.as_view(), name='tag-create'),
-    path('tags/<int:pk>/update/', EventTagUpdateView.as_view(), name='tag-update'),
-    path('tags/<int:pk>/delete/', EventTagDeleteView.as_view(), name='tag-delete'),
-    
+    path('tags/<slug:slug>/update/', EventTagUpdateView.as_view(), name='tag-update'),  # Bug 3
+    path('tags/<slug:slug>/delete/', EventTagDeleteView.as_view(), name='tag-delete'),  # Bug 3
+
     # Registration URLs
     path('registrations/', EventRegistrationListView.as_view(), name='registration-list'),
     path('registrations/create/', EventRegistrationCreateView.as_view(), name='registration-create'),
-    path('registrations/<int:pk>/', EventRegistrationDetailView.as_view(), name='registration-detail'),
-    path('registrations/<int:pk>/cancel/', EventRegistrationCancelView.as_view(), name='registration-cancel'),
+    path('registrations/<str:confirmation_code>/', EventRegistrationDetailView.as_view(), name='registration-detail'),         # Bug 4
+    path('registrations/<str:confirmation_code>/cancel/', EventRegistrationCancelView.as_view(), name='registration-cancel'),  # Bug 4
     path('registrations/<int:pk>/confirm/', EventRegistrationConfirmView.as_view(), name='registration-confirm'),
-    
+
     # Gallery URLs
-    path('gallery/upload/', EventGalleryCreateView.as_view(), name='gallery-upload'),
+    path('<slug:slug>/gallery/upload/', EventGalleryCreateView.as_view(), name='gallery-upload'),  # Bug 5
     path('gallery/<int:pk>/delete/', EventGalleryDeleteView.as_view(), name='gallery-delete'),
 ]
