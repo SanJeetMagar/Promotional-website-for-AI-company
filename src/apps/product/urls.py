3224db7aeee33a72770e3a23c9df8717f1e12745
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-06-24 20:51:56 +0545

    fixed urls.py

--- src/apps/product/urls.py
+++ src/apps/product/urls.py
@@ -1,7 +1,16 @@
-from .views import ProductDropdownListView, ProductDetailView
 from django.urls import path
+from .views import (
+    ProductDropdownListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
 
 urlpatterns = [
     path('', ProductDropdownListView.as_view(), name='product-dropdown-list'),
+    path('create/', ProductCreateView.as_view(), name='product-create'),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
+    path('<slug:slug>/update/', ProductUpdateView.as_view(), name='product-update'),
+    path('<slug:slug>/delete/', ProductDeleteView.as_view(), name='product-delete'),
 ]
