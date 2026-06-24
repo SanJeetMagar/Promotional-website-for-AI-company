54cf8ecfcc1cf5895009562c089cbb3afeae289d
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-06-24 20:14:55 +0545

    added permisision

---
+++ src/apps/blog/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admins to edit objects.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
