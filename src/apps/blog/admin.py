d14df88c8b8c23b6c3c547165551515d1812cc15
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-06-24 20:15:49 +0545

    fix admin.py

--- src/apps/blog/admin.py
+++ src/apps/blog/admin.py
@@ -1,43 +1,94 @@
 from django.contrib import admin
-from .models import BlogPost, Comment, Tag,KeyTakeaway, BlogImage
+from .models import BlogPost, Comment, Tag, BlogImage, KeyTakeaway
 
 
-class KeyTakeawayInline(admin.TabularInline):
-    model = KeyTakeaway
-    extra = 3  # shows 3 empty forms by default
 class BlogImageInline(admin.TabularInline):
     model = BlogImage
     extra = 1
 
+
+class KeyTakeawayInline(admin.TabularInline):
+    model = KeyTakeaway
+    extra = 1
+
+
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'status', 'is_featured', 'published_date', 'view_count']
+    list_filter = ['status', 'is_featured', 'published_date', 'tags']
+    search_fields = ['title', 'content', 'author']
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ['tags']
     inlines = [KeyTakeawayInline, BlogImageInline]
-    list_display = ['title', 'author', 'published_date', 'status', 'is_featured']
-    list_filter = ['status', 'is_featured', 'published_date']
-    search_fields = ['title', 'author', 'content']
-    readonly_fields = ['published_date', 'slug']
-    exclude = ['slug']  # ← remove slug from form entirely
     actions = ['publish_posts', 'archive_posts']
-
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'slug', 'author', 'content', 'excerpt')
+        }),
+        ('Media', {
+            'fields': ('image',)
+        }),
+        ('Settings', {
+            'fields': ('status', 'is_featured', 'order', 'tags')
+        }),
+        ('SEO', {
+            'fields': ('meta_description', 'meta_keywords'),
+            'classes': ('collapse',)
+        }),
+        ('Statistics', {
+            'fields': ('view_count', 'published_date'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    readonly_fields = ['view_count']
+    
     def publish_posts(self, request, queryset):
         for post in queryset:
             post.publish()
-    publish_posts.short_description = 'Publish selected posts'
-
+        self.message_user(request, f"{queryset.count()} posts published successfully.")
+    publish_posts.short_description = "Publish selected posts"
+    
     def archive_posts(self, request, queryset):
         for post in queryset:
             post.archive()
-    archive_posts.short_description = 'Archive selected posts'
+        self.message_user(request, f"{queryset.count()} posts archived successfully.")
+    archive_posts.short_description = "Archive selected posts"
 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):   
+class CommentAdmin(admin.ModelAdmin):
     list_display = ['name', 'blog_post', 'published_date', 'is_active']
-    list_filter = ['is_active']
-    search_fields = ['name', 'content']
+    list_filter = ['is_active', 'published_date']
+    search_fields = ['name', 'email', 'content']
+    actions = ['approve_comments', 'disapprove_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(is_active=True)
+        self.message_user(request, f"{queryset.count()} comments approved.")
+    approve_comments.short_description = "Approve selected comments"
+    
+    def disapprove_comments(self, request, queryset):
+        queryset.update(is_active=False)
+        self.message_user(request, f"{queryset.count()} comments disapproved.")
+    disapprove_comments.short_description = "Disapprove selected comments"
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['name']
-    search_fields = ['name']    
+    list_display = ['name', 'slug']
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(BlogImage)
+class BlogImageAdmin(admin.ModelAdmin):
+    list_display = ['blog_post', 'caption', 'order']
+    list_filter = ['blog_post']
+
+
+@admin.register(KeyTakeaway)
+class KeyTakeawayAdmin(admin.ModelAdmin):
+    list_display = ['blog_post', 'content', 'order']
+    list_filter = ['blog_post']
